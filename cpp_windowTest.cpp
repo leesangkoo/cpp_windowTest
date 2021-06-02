fb1f220b8e6e94acbc5fbcc97f05ac723c615a6c
sklee <mindmoversklee@gmail.com>
2021-06-02 23:48:23 +0900

    초기 커밋.

---
+++ cpp_windowTest.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <time.h>
+using namespace sf;
+
+//SFML 2.5.1 을 설치해줘야 실행가능
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "The Game");
+    sf::Event event;
+
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear(Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
+
